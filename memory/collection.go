dc4ee3bd2eedf89f96303bef4296646eec7cade9
Sean Johnson <sean.johnson.arizona@gmail.com>
2022-10-13 14:39:32 -0700

    Moved packages.

--- memory/collection.go
+++ memory/collection.go
@@ -3,22 +3,26 @@ package collection
 import (
 	"context"
 	"fmt"
+
+	"github.com/cardboardrobots/mongodata/repository"
 )
 
 type Collection[T any] struct {
-	Items     map[string]T
-	currentId int
+	Items             map[string]T
+	currentId         int
+	preInsertCallback func(value T, id string)
 }
 
-func NewCollection[T any]() Collection[T] {
-	return Collection[T]{
-		Items: map[string]T{},
+func NewCollection[T any](preInsertCallback func(value T, id string)) repository.Repository[T] {
+	return &Collection[T]{
+		Items:             map[string]T{},
+		preInsertCallback: preInsertCallback,
 	}
 }
 
-func (c *Collection[T]) GetList(ctx context.Context) (ListResult[T], error) {
+func (c *Collection[T]) GetList(ctx context.Context) (repository.ListResult[T], error) {
 	count, _ := c.Count(ctx)
-	return NewListResult(count, c.Slice()), nil
+	return repository.NewListResult(count, c.Slice()), nil
 }
 
 func (c *Collection[T]) Count(ctx context.Context) (int, error) {
@@ -39,28 +43,24 @@ func (c *Collection[T]) GetById(ctx context.Context, id string) (T, error) {
 	value, ok := c.Items[id]
 	if !ok {
 		var zero T
-		return zero, NewErrNotFound()
+		return zero, repository.NewErrNotFound()
 	}
 
 	return value, nil
 }
 
-func (c *Collection[T]) Insert(value T, callback func(value T, id string)) string {
+func (c *Collection[T]) Insert(ctx context.Context, value T) (string, error) {
 	id := fmt.Sprintf("%v", c.currentId)
 	c.currentId = c.currentId + 1
-	callback(value, id)
+	c.preInsertCallback(value, id)
 	c.Items[id] = value
-	return id
-}
-
-func (c *Collection[T]) InsertAtId(id string, value T) {
-	c.Items[id] = value
+	return id, nil
 }
 
 func (c *Collection[T]) Update(ctx context.Context, id string, value T) error {
 	_, ok := c.Items[id]
 	if !ok {
-		return NewErrNotFound()
+		return repository.NewErrNotFound()
 	}
 
 	c.Items[id] = value
@@ -70,7 +70,7 @@ func (c *Collection[T]) Update(ctx context.Context, id string, value T) error {
 func (c *Collection[T]) Delete(ctx context.Context, id string) error {
 	_, ok := c.Items[id]
 	if !ok {
-		return NewErrNotFound()
+		return repository.NewErrNotFound()
 	}
 
 	delete(c.Items, id)
