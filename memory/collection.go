87a74f76fa454bb2704c8cd5ab76c31f99ada563
Sean Johnson <sean.johnson.arizona@gmail.com>
2022-10-14 12:40:25 -0700

    Fixed packages and test.

--- memory/collection.go
+++ memory/collection.go
@@ -1,4 +1,4 @@
-package collection
+package memory
 
 import (
 	"context"
--- memory/collection_test.go
+++ memory/collection_test.go
@@ -1,4 +1,4 @@
-package collection
+package memory
 
 import (
 	"context"
@@ -7,7 +7,7 @@ import (
 
 func TestLength(t *testing.T) {
 	ctx := context.TODO()
-	c := NewCollection[string](func(value, id string) {})
+	c := NewCollection(func(value, id string) {})
 	length, _ := c.Count(ctx)
 	if length != 0 {
 		t.Errorf("Length should be 0, received: %v", length)
@@ -20,7 +20,7 @@ func TestLength(t *testing.T) {
 }
 
 // func TestSlice(t *testing.T) {
-// 	c := NewCollection[string](func(value, id string) {})
+// 	c := NewCollection(func(value, id string) {})
 
 // 	slice := c.Slice()
 // 	length := len(slice)
@@ -44,7 +44,7 @@ func TestLength(t *testing.T) {
 
 func TestGet(t *testing.T) {
 	ctx := context.TODO()
-	c := NewCollection[string](func(value, id string) {})
+	c := NewCollection(func(value, id string) {})
 	want := "abcde"
 	id, err := c.Insert(ctx, want)
 	if err != nil {
@@ -63,18 +63,18 @@ func TestGet(t *testing.T) {
 
 func TestInsert(t *testing.T) {
 	ctx := context.TODO()
-	c := NewCollection[string]()
 	valueCallback := ""
 	idCallback := ""
-	id0 := c.Insert("abcde", func(value, id string) {
+	c := NewCollection(func(value, id string) {
 		valueCallback = value
 		idCallback = id
 	})
-	id1 := c.Insert("fghij", func(value, id string) {})
+	id0, _ := c.Insert(ctx, "abcde")
+	id1, _ := c.Insert(ctx, "fghij")
 	if id0 == "" || id1 == "" || id0 == id1 {
 		t.Errorf("ids should be non-nil, and unique.  Received: %v, %v", id0, id1)
 	}
-	if valueCallback != "abcde" || idCallback != id0 {
+	if valueCallback != "fghij" || idCallback != id1 {
 		t.Errorf("Callback is wrong.  Received %v, %v", valueCallback, idCallback)
 	}
 	length, _ := c.Count(ctx)
@@ -86,9 +86,9 @@ func TestInsert(t *testing.T) {
 
 func TestReplace(t *testing.T) {
 	ctx := context.TODO()
-	c := NewCollection[string]()
+	c := NewCollection(func(value, id string) {})
 	want := "fghij"
-	id := c.Insert("abcde", func(value, id string) {})
+	id, _ := c.Insert(ctx, "abcde")
 
 	err := c.Update(ctx, id, want)
 	if err != nil {
@@ -107,7 +107,7 @@ func TestReplace(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	ctx := context.TODO()
-	c := NewCollection[string](func(value, id string) {})
+	c := NewCollection(func(value, id string) {})
 	id, err := c.Insert(ctx, "abcde")
 	if err != nil {
 		t.Errorf("%v", err)
@@ -124,16 +124,16 @@ func TestDelete(t *testing.T) {
 	}
 }
 
-func sliceEqual[T comparable](a []T, b []T) bool {
-	if len(a) != len(b) {
-		return false
-	}
+// func sliceEqual[T comparable](a []T, b []T) bool {
+// 	if len(a) != len(b) {
+// 		return false
+// 	}
 
-	for i, value := range a {
-		if value != b[i] {
-			return false
-		}
-	}
+// 	for i, value := range a {
+// 		if value != b[i] {
+// 			return false
+// 		}
+// 	}
 
-	return true
-}
+// 	return true
+// }
